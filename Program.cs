5bb237c3f03754bb5a222f961f8f7db1b4494492
Igor Baranov <charsequence@gmail.com>
2021-04-24 12:49:16 +0300

    ez RLE

--- Program.cs
+++ Program.cs
@@ -16,12 +16,50 @@ namespace ImageCompression
 
             string sourceFilename = args[0];
             string targetFilename = args[1];
+            string resultFilename = "result.bmp";
 
-            FileStream fs = new FileStream(sourceFilename, FileMode.Open);
-            byte[] compressedImage = new RleCompressor().Compress(fs);
+            FileStream fsSource = new FileStream(sourceFilename, FileMode.Open);
+            long sourceLen = fsSource.Length;
+            byte[] compressedImage = new RleCompressor().Compress(fsSource);
+            fsSource.Close();
             File.WriteAllBytes(targetFilename, compressedImage);
 
+
+            FileStream fsTarget = new FileStream(targetFilename, FileMode.Open);
+            byte[] decompressedImage = new RleCompressor().Decompress(fsTarget);
+            fsTarget.Close();
+            File.WriteAllBytes(resultFilename, decompressedImage);
+
+            Console.WriteLine("Compression is: " + (double)sourceLen / compressedImage.Length);
+            if (Compare(sourceFilename, "result.bmp") != 0)
+                Console.WriteLine("Files not equals!");
+            else
+                Console.WriteLine("Correct!");
+
+            Console.ReadKey();
             return 0;
+        }
+
+        static int Compare(string sourceFilename, string resultFileName)
+        {
+            byte[] src = File.ReadAllBytes(sourceFilename);
+            byte[] rst = File.ReadAllBytes("result.bmp");
+            int result = 0;
+            for (int i = 0; i < src.Length || i < rst.Length; i++)
+            {
+                if (i >= src.Length || i >= rst.Length || src[i] != rst[i])
+                {
+                    result = 1;
+                    int a = -1;
+                    int b = -1;
+                    if (i < src.Length)
+                        a = src[i];
+                    if (i < rst.Length)
+                        b = rst[i];
+                    Console.WriteLine($"{i}:\t{a}\t{b}");
+                }
+            }
+            return result;
         }
     }
 }
--- RleCompressor.cs
+++ RleCompressor.cs
@@ -1,65 +1,105 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Text;
 
 namespace ImageCompression
 {
+    struct BmpHeader
+    {
+        public ushort Signature; // 2 bytes
+        public uint Size; // 4 bytes
+        // 2 bytes reserved
+        // 2 bytes reserved
+        public uint Offset; // 4 bytes
+
+        public BmpHeader(byte[] arr)
+        {
+            Signature = BitConverter.ToUInt16(arr, 0);
+            Size = BitConverter.ToUInt32(arr, 2);
+            Offset = BitConverter.ToUInt32(arr, 10);
+        }
+    }
     class RleCompressor //: ICompressor
     {
         public byte[] Compress(FileStream image)
         {
             // check format
-
             List<byte> result = new List<byte>();
-            byte currentColor = 0;
-            byte row = 0;
+
 
             // cut headers
             byte BmpHeaderSize = 14;
-            byte[] header = new byte[BmpHeaderSize];
-            image.Read(header, 0, BmpHeaderSize);
+            byte[] arrHeader = new byte[BmpHeaderSize];
+            image.Read(arrHeader, 0, BmpHeaderSize);
+            var header = new BmpHeader(arrHeader);
+            image.Position = header.Offset;
 
-            bool newChar = true;
-            byte[] buffer = new byte[1];
-            while (image.Read(buffer, 0, 1) > 0)
+            List<Color> difRow = new List<Color>();
+            Color current = Color.FromArgb(0, 0, 0);
+            sbyte row = -1;
+            byte[] buffer = new byte[3];
+            byte[] prev = new byte[3];
+            while (image.Read(buffer, 0, buffer.Length) > 0)
             {
-                if (newChar)
-                {
-                    currentColor = buffer[0];
-                    row = 1;
-                    newChar = false;
-                    continue;
-                }
-
-                if (buffer[0] == currentColor)
+                if (buffer[0] == current.R && buffer[1] == current.G && buffer[2] == current.B && row != 127)
                     row++;
                 else
                 {
-                    result.Add(row);
-                    result.Add(currentColor);
-                    currentColor = buffer[0];
-                    row = 0;
-                    newChar = true;
+                    if (row < 0)
+                    {
+                        difRow.Add(Color.FromArgb(buffer[0], buffer[1], buffer[2]));
+                        row--;
+                    }
+                    else if (row > 0)
+                    {
+                        result.Add((byte)row);
+                        result.Add((byte)current.R);
+                        result.Add((byte)current.G);
+                        result.Add((byte)current.B);
+                    }
+                    current = Color.FromArgb(buffer[0], buffer[1], buffer[2]);
+                    row = 1;
                 }
-
-                if (row == 255)
-                {
-                    result.Add(row);
-                    result.Add(currentColor);
-                    newChar = true;
-                }
-
+                prev[0] = buffer[0];
+                prev[1] = buffer[1];
+                prev[2] = buffer[2];
             }
 
-
+            result.Add((byte)row);
+            result.Add(current.R);
+            result.Add(current.G);
+            result.Add(current.B);
 
             return result.ToArray();
         }
 
-        public byte[] Decompress(byte[] image)
+        public byte[] Decompress(FileStream image)
         {
-            throw new NotImplementedException();
+            List<byte> result = new List<byte>();
+            //byte[] header = { 66, 77, 54, 0, 48, 0, 0, 0, 0, 0, 54, 0, 0, 0, 40, 0,
+            //                  0, 0, 0, 4, 0, 0, 0, 4, 0, 0, 1, 0, 24, 0, 0, 0,
+            //                  0, 0, 0, 0, 48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            //                  0, 0, 0, 0, 0, 0 };
+            byte[] header = { 66, 77, 138, 0, 192, 0, 0, 0, 0, 0, 138, 0, 0, 0, 124, 0,
+                              0, 0, 0, 8, 0, 0, 0, 8, 0, 0, 1, 0, 24, 0, 0, 0,
+                              0, 0, 0, 0, 192, 0, 18, 11, 0, 0, 18, 11, 0, 0, 0, 0,
+                              0, 0, 0, 0, 0, 0 };
+            foreach (byte h in header)
+                result.Add(h);
+
+            byte[] buffer = new byte[4];
+            while (image.Read(buffer, 0, buffer.Length) > 0)
+            {
+                for (int i = 0; i < buffer[0]; i++)
+                {
+                    result.Add(buffer[1]);
+                    result.Add(buffer[2]);
+                    result.Add(buffer[3]);
+                }
+            }
+            return result.ToArray();
         }
     }
 }
